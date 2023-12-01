d5794fb87e61717fd1ec4d2f485a9dec25dd4919
Mr615-TN <tneema501@gmail.com>
2023-12-01 13:04:07 -0600

    update

--- Day1/code.py
+++ Day1/code.py
@@ -1,7 +1,19 @@
 def calculateSum(calibration_txt):
     sum = 0
-    
+    word_to_digit = {
+    'one': '1',
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9'
+    }
     for line in calibration_txt:
+        for word, digit in word_to_digit.items():
+            line = line.replace(word, digit)
         # Find the first and last digits and combine them to form a two-digit number
         first_digit = next(char for char in line if char.isdigit())
         last_digit = next(char for char in reversed(line) if char.isdigit())
@@ -19,7 +31,7 @@ def calibration_file(file_path):
         print(f"Error: File '{file_path}' not found.")
         return None
 
-file_path = 'puzzleInput.txt'
+file_path = 'Input.txt'
 calibration_document = calibration_file(file_path)
 if calibration_document:
     result = calculateSum(calibration_document)
